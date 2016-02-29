e0437b943aaac7b684a6e3316baaac0ff0cfc166
skofman <shlomo.kofman@gmail.com>
2016-02-29 11:10:05 -0800

    Adding feature that populates the front page with random items and uploading item images

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+//Creating a category variable, defaults to "All".
 var category = document.getElementById('category').innerText.trim();
-
+//Creating a category selector.
 document.getElementsByClassName('cat-selector')[0].addEventListener("click", function() {
   category = "All";
   document.getElementById('category').innerHTML = 'All <span class="caret"></span>';
@@ -19,3 +20,19 @@ document.getElementsByClassName('cat-selector')[3].addEventListener("click", fun
   category = "Phones";
   document.getElementById('category').innerHTML = 'Phones <span class="caret"></span>';
 });
+//Populate front page with random items.
+var titleElements = document.getElementsByClassName('main-title');
+var imageElements = document.getElementsByClassName('main-image');
+var randomItems = [];
+
+for (var i = 0; i < 9; i++) {
+  var randomItem = Math.floor(30 * Math.random());
+  if (randomItems.indexOf(randomItem) === -1) {
+    randomItems.push(randomItem);
+    titleElements[i].innerHTML = items[randomItem].title;
+    imageElements[i].setAttribute("src", items[randomItem].image);
+  }
+  else {
+    i--;
+  }
+}
